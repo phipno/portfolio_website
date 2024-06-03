b0ae7935ac281feb4d0a446dd99ee2d0e2fa6d6e
phipno <phipno@gmail.com>
2024-06-03 16:48:08 +0200

    implemented the star wars crawl

--- stars.js
+++ stars.js
@@ -9,27 +9,30 @@ function initCosmos() {
   cnv.height = H;
   ctx.fillStyle = "#112";
   ctx.shadowBlur = 10;
-  ctx.shadowColor = "yellow";
+  ctx.shadowColor = "grey";
   // Start animation
   animate(ctx, W, H);
 }
 
 function animate(ctx, W, H) {
   // Random position and size of stars
-  let x = W * Math.random();
-  let y = H * Math.random();
-  let r = 2.5 * Math.random();
-  let alpha = Math.random();
-  // Draw the stars
-  ctx.globalAlpha = alpha;
-  ctx.beginPath();
-  ctx.fillStyle = "white";
-  ctx.arc(x, y, r, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.globalAlpha = 1;
+  for (let i = 0; i < 4000; i++) {
+
+    let x = W * Math.random();
+    let y = H * Math.random();
+    let r = 2.5 * Math.random();
+    let alpha = Math.random();
+    // Draw the stars
+    ctx.globalAlpha = alpha;
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.globalAlpha = 1;
+  }
 
   // Using setTimeout instead of window.requestAnimationFrame for slower speed
-  setTimeout(() => animate(ctx, W, H), 100);
+  // setTimeout(() => animate(ctx, W, H), 100);
 }
 
 // Call initCosmos when the content is loaded
