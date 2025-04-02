2282f7ba3e434b58eab93377b517e5df31a7fb24
Philipp Nolte <phipno@gmail.com>
2025-04-02 18:14:13 +0000

    change swipe distance

--- js/module.js
+++ js/module.js
@@ -29,22 +29,41 @@ window.play = play;
 window.fire = fire;
 window.moveShip = moveShip;
 
-let touchstartX = 0
-let touchendX = 0
-    
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
 function checkDirection() {
-  if (touchendX < touchstartX) ;
-  if (touchendX > touchstartX + 70) changeContent("home");
+  const deltaX = touchendX - touchstartX;
+  const deltaY = touchendY - touchstartY;
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    if (deltaX > 70) {
+      changeContent("home"); 
+    } else if (deltaX < -70) {
+      console.log("Swipe nach links erkannt");
+    }
+  } else {
+    // Vertikaler Swipe
+    if (deltaY > 70) {
+      console.log("Swipe nach unten erkannt");
+    } else if (deltaY < -70) {
+      console.log("Swipe nach oben erkannt");
+    }
+  }
 }
 
-document.addEventListener('touchstart', e => {
+document.addEventListener("touchstart", (e) => {
   touchstartX = e.changedTouches[0].screenX;
-})
+  touchstartY = e.changedTouches[0].screenY;
+});
 
-document.addEventListener('touchend', e => {
+document.addEventListener("touchend", (e) => {
   touchendX = e.changedTouches[0].screenX;
+  touchendY = e.changedTouches[0].screenY;
   checkDirection();
-})
+});
 
 if (!detectPortraitMode()) {
   const gameElement = document.querySelector(".game");
